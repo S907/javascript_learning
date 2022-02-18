ee781266d8a36a78a2c9182dc1d560ae529ec528
unknown <souvikghosh146@gmail.com>
2022-02-18 12:12:33 +0530

    callbac

---
+++ callbacks/index.js
@@ -0,0 +1,27 @@
+// using Settimeout
+
+
+// setTimeout(function(){
+
+//     console.log("Someone was here-- TIMER");
+    
+// }, 5000);
+
+
+
+// First Callback function
+
+// function newOne(insideOneCallback){
+//     console.log("Invocation from the main function");
+//     insideOneCallback();
+
+// }
+
+// newOne(function insideOneCallback(){
+//     console.log("Invocation from the callback function");
+// })  
+
+// insideOneCallback is a callback function
+
+//--------------------------------------------
+
--- prototypes/protofilter.js
+++ prototypes/protofilter.js
@@ -5,9 +5,6 @@ Array.prototype.myFilter= function(cb){
             pushArr.push(cb(this[i]));
     }
     return pushArr;
-    
-   
-   
 }
 
 // Return statement otherwise you see undefined today at 17.02.2022 //  missed the undefined and became late
